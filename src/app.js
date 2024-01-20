71c71eba67281060504574ccee783982d2ae37b7
JohnRM992 <jonatanrm92@gmail.com>
2024-01-20 17:19:03 -0300

    Se actualiza wireframe,archivos/carpetas para desarrollar la web

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3300;
+
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+})

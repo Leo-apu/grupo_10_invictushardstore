82ffb2d30351dd03b26c8c0da7c75e10af028aa4
LeoVCruz <leovcruz0304@gmail.com>
2024-01-22 05:50:36 -0300

    add login page and login style.

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
 app.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/registro.html'));
 })

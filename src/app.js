dc30122a31d2d021fc24d81f5a998bdca416d77d
Jonathan R. Mascareño <jonatanrm92@gmail.com>
2024-02-04 22:22:56 -0300

    Implementación de Partials,Separación,Template engine

--- src/app.js
+++ src/app.js
@@ -15,11 +15,9 @@ app.listen(port, () => console.log(`Listening on port http://localhost:${port}`)
 //     res.sendFile(path.join(__dirname, '/views/index.html'));
 // })
 
-<<<<<<< Updated upstream
-app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/registro.html'));
-})
-=======
+// app.get('/registro', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/registro.html'));
+// })
 // app.get('/login', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/views/login.html'));
 // })
@@ -27,7 +25,6 @@ app.get('/registro', (req, res) => {
 // app.get('/registro', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/views/registro.html'));
 // })
->>>>>>> Stashed changes
 
 // app.get('/detalleProducto', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/views/productDetail.html'));

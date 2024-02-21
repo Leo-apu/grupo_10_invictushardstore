7fdc8d23059d05281ede14af46d0fb8314eb0966
LeoVCruz <leovcruz0304@gmail.com>
2024-02-21 04:49:57 -0300

    add JSON user

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const data = require('../models/indexProductData.json'); 
+
+const productsFilePath = path.join(__dirname, '../models/indexProductData.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
 	index: (req, res) => {
-		res.render('index', {products: data.results});
+		res.render('index', {products: products.results});
 	},
 	search: (req, res) => {
 	},	
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const usersFilePath = path.join(__dirname, '../data/usersDB.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const controller = {
 	login: (req, res) => {
 		res.render('login');
